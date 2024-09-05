b2f9f31bdb1dcd504a06f0aa5dffab77e5618268
Alberto <alberto@simionato.org>
2024-09-05 21:29:23 +0200

    A few configurations to get the project ready for Heroku deployment

--- server/index.js
+++ server/index.js
@@ -5,7 +5,8 @@ const http = require('http');
 const { Server } = require('socket.io');
 const dotenv = require('dotenv');
 const routes = require('./routes/routes');
-const Robot = require('./models/Robot'); // Modello del Robot per la teleoperazione
+const Robot = require('./models/Robot');
+const path = require('path');
 
 dotenv.config();
 
@@ -17,20 +18,35 @@ const io = new Server(server, {
   },
 });
 
+// Enable CORS and JSON parsing
 app.use(cors());
 app.use(express.json());
 
-// Connessione a MongoDB
-mongoose.connect(process.env.MONGODB_URI)
+// MongoDB Connection
+const mongoURI = process.env.MONGODB_URI || 'fallback-mongo-uri'; 
+mongoose.connect(mongoURI/*, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}*/)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => {
     console.error('MongoDB connection error:', err.message);
-    process.exit(1); // Uscita se non riesce a connettersi a MongoDB
+    process.exit(1); // Exit if unable to connect to MongoDB
   });
 
+// API routes
 app.use('/api', routes);
 
-// Gestione WebSocket
+// Serve static assets from the React frontend build folder in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+}
+
+// WebSocket management
 io.on('connection', (socket) => {
   console.log('A user connected');
 
@@ -49,7 +65,7 @@ io.on('connection', (socket) => {
     try {
       const robot = await Robot.findById(robotId);
       if (!robot) return;
-      
+
       const moveAmount = 20;
       const maxPosition = 380; // Assuming 400 is the boundary
 
@@ -72,7 +88,7 @@ io.on('connection', (socket) => {
 
       await robot.save();
 
-      // Broadcast della nuova posizione a tutti i client connessi
+      // Broadcast new position to all connected clients
       io.emit('robot_position', { pose_x: robot.pose_x, pose_y: robot.pose_y });
     } catch (error) {
       console.error('Error moving robot:', error.message);
@@ -84,5 +100,6 @@ io.on('connection', (socket) => {
   });
 });
 
+// Start the server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
